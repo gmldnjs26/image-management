92a6e3174650aca6de5f474a4a196ee69df2b9b1
gmldnjs26 <imleesky265@gmail.com>
2022-01-11 22:46:40 +0900

    feat: bcryptjs->password + salt

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,24 @@
 const { Router } = require('express')
 const userRouter = Router();
 const User = require("../models/User")
+const { hash } = require('bcryptjs')
 
 userRouter.post("/register", async (req, res) => {
-  console.log(req.body)
-  const user = await new User(req.body).save()
-  res.json({message: 'user registered'})
+  try {
+    if (req.body.password.length < 6) throw new Error('password is too short')
+    if (req.body.username.length < 3) throw new Error("username is too short")
+    const hashedPassword = await hash(req.body.password, 10)
+    console.log(hashedPassword)
+    const user = await new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: hashedPassword,
+    }).save()
+    res.json({message: 'user registered'})
+  } catch(err) {
+    res.status(400).json({ message: err.message })
+  }
+  
 })
  
 module.exports = { userRouter }
