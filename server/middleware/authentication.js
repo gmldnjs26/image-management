18b1cdfb1542e29095a68279068fe5794ef607ce
gmldnjs26 <imleesky265@gmail.com>
2022-01-14 22:22:09 +0900

    server: add middleware authentication

---
+++ server/middleware/authentication.js
@@ -0,0 +1,14 @@
+const User = require("../models/User")
+const mongoose = require('mongoose')
+
+const authenticate = async (req, res, next) => {
+  const { sessionid } = req.headers
+  // DB에 부하를 주기전에 최소한의 체크를 해본다.
+  if (!sessionid || !mongoose.isValidObjectId(sessionid)) return next()
+  const user = await User.findOne({ "sessions._id": sessionid })
+  if(!user) return next()
+  req.uesr = user
+  return next()
+}
+
+module.exports = authenticate
