71a70c7f3a09561ba57c3c4321509953e0f9efef
gmldnjs26 <imleesky265@gmail.com>
2022-01-28 22:02:43 +0900

    feat: add routes me/images public/images

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -1,51 +1,50 @@
-const { Router } = require('express')
-const imageRouter = Router()
-const Image = require('../models/Image')
-const upload = require('../utils/imageUpload')
+const { Router } = require("express");
+const imageRouter = Router();
+const Image = require("../models/Image");
+const upload = require("../utils/imageUpload");
 
-imageRouter.post('/',　upload.single("image"), async (req, res) => {
+imageRouter.post("/", upload.single("image"), async (req, res) => {
   // 유저정보, public유무 확인
   try {
-    if(!req.user) throw new Error("권한이 없습니다.")
-    const image = await new Image({ 
+    if (!req.user) throw new Error("권한이 없습니다.");
+    const image = await new Image({
       user: {
-        // id로 불러내면 _id로 저장된 ObjectId에서 자동으로 String형태로 전환된다. 
+        // id로 불러내면 _id로 저장된 ObjectId에서 자동으로 String형태로 전환된다.
         // 근데 _id는 ObjectId가 type인데 string으로 저장해도 되냐? 몽구스가 알아서 처리해준다
         // 그리고 ObjectId로 안하면 string으로 되버리기 때문에 용량이 더 커지게 된다 ObjectId가 더 효율적
-        _id: req.user.id, 
+        _id: req.user.id,
         name: req.user.name,
-        username: req.user.username
+        username: req.user.username,
       },
       public: req.body.public,
-      key: req.file.filename, 
-      originalFileName: req.file.originalname 
-    }).save()
+      key: req.file.filename,
+      originalFileName: req.file.originalname,
+    }).save();
     res.json(image);
-
-  } catch(err) {
-    console.error(err)
-    res.status(400).json({ message: err.message })
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
   }
-})
+});
 imageRouter.get("/", async (req, res) => {
   // public 이미지 제공
-  const images = await Image.find();
-  res.json(images)
-})
+  const images = await Image.find({ public: true });
+  res.json(images);
+});
 
 imageRouter.delete("/:imageId", (req, res) => {
   // 유저 권한 확인
   // 사진 삭제
 });
 
-imageRouter.patch('/:imageId/like', (req, res) => {
+imageRouter.patch("/:imageId/like", (req, res) => {
   // 유저 권한 확인
   // like 중복 안되도록 확인
-})
+});
 
-imageRouter.patch('/:imageId/unlike', (req, res) => {
+imageRouter.patch("/:imageId/unlike", (req, res) => {
   // 유저 권한 확인
   // like 중복 취소 안되도록 확인
-})
+});
 
-module.exports = imageRouter
+module.exports = imageRouter;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,83 +1,95 @@
-const { Router } = require('express')
+const { Router } = require("express");
 const userRouter = Router();
-const User = require("../models/User")
-const { hash, compare } = require('bcryptjs')
-const mongoose = require('mongoose')
+const User = require("../models/User");
+const { hash, compare } = require("bcryptjs");
+const Image = require("../models/Image");
 
 userRouter.post("/register", async (req, res) => {
   try {
-    if (req.body.password.length < 6) throw new Error('password is too short')
-    if (req.body.username.length < 3) throw new Error("username is too short")
-    const hashedPassword = await hash(req.body.password, 10)
-    console.log(hashedPassword)
+    if (req.body.password.length < 6) throw new Error("password is too short");
+    if (req.body.username.length < 3) throw new Error("username is too short");
+    const hashedPassword = await hash(req.body.password, 10);
+    console.log(hashedPassword);
     const user = await new User({
       name: req.body.name,
       username: req.body.username,
       password: hashedPassword,
-      sessions: [{ createdAt: new Date() }]
-    }).save()
-    const session = user.sessions[0]
-    res.json({ message: 'user registered', sessionID: session._id, name: user.name })
-  } catch(err) {
-    res.status(400).json({ message: err.message })
+      sessions: [{ createdAt: new Date() }],
+    }).save();
+    const session = user.sessions[0];
+    res.json({
+      message: "user registered",
+      sessionID: session._id,
+      name: user.name,
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
-// post -> patch 
-userRouter.patch("/login", async(req, res) => {
+// post -> patch
+userRouter.patch("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if(!user) throw new Error("Failed")
-    const isValid = await compare(req.body.password, user.password)
-    if(!isValid) throw new Error("Failed")
-    user.sessions.push({ createdAt: new Date() })
-    const session = user.sessions[user.sessions.length - 1]
-    await user.save()
+    if (!user) throw new Error("Failed");
+    const isValid = await compare(req.body.password, user.password);
+    if (!isValid) throw new Error("Failed");
+    user.sessions.push({ createdAt: new Date() });
+    const session = user.sessions[user.sessions.length - 1];
+    await user.save();
     res.json({
       message: "user validated",
       sessionId: session._id,
       name: user.name,
-      userId: user._id
-    })
-  } catch(err) {
-    res.status(400).json({ message: err.message })
+      userId: user._id,
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
-userRouter.patch("/logout", async(req, res) => {
+userRouter.patch("/logout", async (req, res) => {
   try {
-    if(!req.user) throw new Error("invalid session id")
+    if (!req.user) throw new Error("invalid session id");
     await User.updateOne(
       { _id: req.user.id },
       { $pull: { sessions: { _id: req.headers.sessionid } } } // mongoDB query language
-    )
-    res.json({ message: "user is logged out" })
-  } catch(err) {
+    );
+    res.json({ message: "user is logged out" });
+  } catch (err) {
     res.status(400).json({
-      message: err.message
-    })
+      message: err.message,
+    });
   }
-})
+});
 
-userRouter.get("/me", async(req, res) => {
+userRouter.get("/me", async (req, res) => {
   try {
-    if(!req.user) throw new Error("권한이 없습니다.")
+    if (!req.user) throw new Error("권한이 없습니다.");
     res.json({
       message: "success",
       sessionId: req.headers.sessionid,
       name: req.user.name,
-      userId: req.user._id
-    })
-  } catch(err) {
-    console.error(err)
+      userId: req.user._id,
+    });
+  } catch (err) {
+    console.error(err);
     res.status(400).json({
-      message: err.message
-    })
+      message: err.message,
+    });
   }
-})
+});
 
-userRouter.get('/me', (req, res) => {
+userRouter.get("/me/images", async (req, res) => {
   // 본인의 사진들만 리턴
-})
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    const images = await Image.find({ "user._id": req.user.id });
+    res.json(images);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+});
 
-module.exports = { userRouter }
+module.exports = { userRouter };
