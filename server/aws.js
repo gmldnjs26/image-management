f199568c238d39a67025005c2a9ec7563096386a
gmldnjs26 <imleesky265@gmail.com>
2022-02-21 23:03:09 +0900

    server: s3

---
+++ server/aws.js
@@ -0,0 +1,10 @@
+const { AWS_SECRET_KEY, AWS_ACCESS_KEY } = process.env;
+
+const aws = require("aws-sdk");
+
+const s3 = new aws.S3({
+  secretAccessKey: AWS_SECRET_KEY,
+  accessKeyId: AWS_ACCESS_KEY,
+});
+
+module.exports = { s3 };
--- server/utils/imageUpload.js
+++ server/utils/imageUpload.js
@@ -1,24 +1,34 @@
-const multer = require('multer')
-const { v4: uuid } = require('uuid')
-const mime = require("mime-types")
+const multer = require("multer");
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types");
+const multerS3 = require("multer-s3");
+const { s3 } = require("../aws");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "./uploads"), // 파일에 따라 저장되는 폴더를 다르게 할 수 있다.
-  filename: (req, file, cb) => 
-    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`) // 파일에 따라 저장되는 파일이름을 다르게 할 수 있다.
-})
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => cb(null, "./uploads"), // 파일에 따라 저장되는 폴더를 다르게 할 수 있다.
+//   filename: (req, file, cb) =>
+//     cb(null, `${uuid()}.${mime.extension(file.mimetype)}`) // 파일에 따라 저장되는 파일이름을 다르게 할 수 있다.
+// })
+
+const storage = multerS3({
+  s3,
+  bucket: "image-upload-tutorial",
+  key: (req, file, cb) =>
+    cb(null, `raw/${uuid()}.${mime.extension(file.mimetype)}`),
+});
+
 const upload = multer({
-  storage, 
+  storage,
   fileFilter: (req, file, cb) => {
-    if (["image/png",'image/jpeg'].includes(file.mimetype)) {
-      cb(null, true)
+    if (["image/png", "image/jpeg"].includes(file.mimetype)) {
+      cb(null, true);
     } else {
-      cb(new Error('invalid file types'), false)
+      cb(new Error("invalid file types"), false);
     }
   },
   limits: {
     fileSize: 1024 * 1024 * 5,
-  }
-})
+  },
+});
 
-module.exports = upload
+module.exports = upload;
