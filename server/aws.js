0407f485c9da04fdb0aef6ca9192c8ac209d859a
gmldnjs26 <imleesky265@gmail.com>
2022-02-28 22:12:48 +0900

    server-feat: createPresignedURL

--- server/aws.js
+++ server/aws.js
@@ -2,11 +2,31 @@ const { AWS_SECRET_KEY, AWS_ACCESS_KEY } = process.env;
 
 const aws = require("aws-sdk");
 
-console.log(AWS_SECRET_KEY, AWS_ACCESS_KEY);
-
 const s3 = new aws.S3({
   secretAccessKey: AWS_SECRET_KEY,
   accessKeyId: AWS_ACCESS_KEY,
 });
 
-module.exports = { s3 };
+const getSignedUrl = (key) => {
+  return new Promise((resovle, reject) => {
+    s3.createPresignedPost(
+      {
+        Bucket: "first-image-storage",
+        Fields: {
+          key,
+        },
+        Expires: 300,
+        Conditions: [
+          ["content-length-range", 0, 50 * 1000 * 1000], // 0 ~ 50MB
+          ["starts-with", "$Content-Type", "image/"], // only image
+        ],
+      },
+      (err, data) => {
+        if (err) throw err;
+        return;
+      }
+    );
+  });
+};
+
+module.exports = { s3, getSignedUrl };
