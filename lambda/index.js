cb90d370d9d48cd40e4caac6246e78b155ca0060
gmldnjs26 <imleesky265@gmail.com>
2022-03-13 16:25:38 +0900

    feat: image resizer lambda

---
+++ lambda/index.js
@@ -0,0 +1,52 @@
+const sharp = require("sharp");
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+
+const transformationOptions = [
+  { name: "w140", width: 140 },
+  { name: "w600", width: 600 },
+];
+
+exports.handler = async (event) => {
+  try {
+    const Key = event.Records[0].s3.object.key;
+    const keyOnly = Key.split("/")[1];
+    console.log(`Image Resizing: ${keyOnly}`);
+    const image = await s3
+      .getObject({ Bucket: "first-image-storage", Key })
+      .promise();
+    console.log("Get Image Success", image);
+    await Promise.all(
+      transformationOptions.map(async ({ name, width }) => {
+        try {
+          const newKey = `${name}/${keyOnly}`;
+          console.log("newKey", newKey);
+
+          const resizedImage = await sharp(image.Body)
+            .rotate()
+            .resize(width)
+            .toBuffer();
+          await s3
+            .putObject({
+              Bucket: "first-image-storage",
+              Body: resizedImage,
+              Key: newKey,
+            })
+            .promise();
+        } catch (err) {
+          throw err;
+        }
+      })
+    );
+    return {
+      statusCode: 200,
+      body: event,
+    };
+  } catch (err) {
+    console.log("!!!!!!!!!!!!Error: ", err);
+    return {
+      statusCode: 500,
+      body: event,
+    };
+  }
+};
